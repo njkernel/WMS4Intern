39a2273eb1e303bea07f76b79e499dbfc0407987
xmxxmxxmxxmxxmx <1695205904@qq.com>
2019-01-15 18:51:32 +0800

    删除classes

--- classes/production/wms/static/js/specific/order-detail.js
+++
@@ -1,7 +0,0 @@
-window.onload=function(){
-       var button_show=document.getElementById("button_show");
-       var detail_input=document.getElementById("detail_input");
-       button1.onclick=function(){
-        detail_input.style.display="block"; 
-       }
-    }   
