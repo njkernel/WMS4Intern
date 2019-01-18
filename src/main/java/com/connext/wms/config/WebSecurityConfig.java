175ddb50034b7cced2c77b47abb314f9879b5256
Anymre <36187602+Anymre@users.noreply.github.com>
2019-01-18 18:02:51 +0800

    Marcus bug修复和代码优化 (#38)
    * 状态选择框修复
    
    * index记住密码启用
    
    * 优化收获异常流程

--- src/main/java/com/connext/wms/config/WebSecurityConfig.java
+++ src/main/java/com/connext/wms/config/WebSecurityConfig.java
@@ -15,42 +15,44 @@ import java.io.PrintWriter;
  * @Date: 2018/12/21 10:18
  * @Version 1.0
  */
-//@Configuration
+@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-  @Autowired
-  private UserDetailServiceImpl userDetailService;
+    @Autowired
+    private UserDetailServiceImpl userDetailService;
 
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(userDetailService).passwordEncoder(new BCryptPasswordEncoder());
-  }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailService).passwordEncoder(new BCryptPasswordEncoder());
+    }
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable()
-            .authorizeRequests()
-            .antMatchers("/js/**", "/css/**", "/img/**", "/user/login").permitAll()
-            .antMatchers("/api/**").permitAll()
-            .antMatchers("/user/**").hasAnyAuthority("admin")
-            .anyRequest().authenticated()
-            .and()
-            .formLogin().loginPage("/user/login").successHandler((httpServletRequest, httpServletResponse, authentication) -> {
-      httpServletResponse.setContentType("application/json;charset=utf-8");
-      PrintWriter out = httpServletResponse.getWriter();
-      out.write("{\"state\":\"Access\"}");
-      out.flush();
-      out.close();
-    }).failureHandler((httpServletRequest, httpServletResponse, e) -> {
-      httpServletResponse.setContentType("application/json;charset=utf-8");
-      PrintWriter out = httpServletResponse.getWriter();
-      out.write("{\"state\":\"" + "账号或密码错误" + "\"}");
-      out.flush();
-      out.close();
-    })
-            .permitAll()
-            .and()
-            .logout()
-            .logoutUrl("/user/outLogin").permitAll()
-            .and().headers().frameOptions().disable();
-  }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/js/**", "/css/**", "/img/**", "/user/login").permitAll()
+                .antMatchers("/api/**").permitAll()
+                .antMatchers("/user/**").hasAnyAuthority("admin")
+                .anyRequest().authenticated()
+                .and().rememberMe()
+                .and()
+                .formLogin().loginPage("/user/login").successHandler((httpServletRequest, httpServletResponse, authentication) -> {
+            httpServletResponse.setContentType("application/json;charset=utf-8");
+            PrintWriter out = httpServletResponse.getWriter();
+            out.write("{\"state\":\"Access\"}");
+            out.flush();
+            out.close();
+        }).failureHandler((httpServletRequest, httpServletResponse, e) -> {
+            httpServletResponse.setContentType("application/json;charset=utf-8");
+            PrintWriter out = httpServletResponse.getWriter();
+            out.write("{\"state\":\"" + "账号或密码错误" + "\"}");
+            out.flush();
+            out.close();
+        })
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/user/outLogin").permitAll()
+                .and().headers().frameOptions().disable();
+    }
+
 }
