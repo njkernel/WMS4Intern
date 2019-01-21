3aa268340ec1aa464b0d1cdb94beb9bb283d81e7
Chao.Sun <Chao.Sun@connext.com.cn>
2019-01-21 14:38:30 +0800

    优化分页，解决页面显示bug

--- src/main/java/com/connext/wms/service/impl/ExceptionServiceImpl.java
+++ src/main/java/com/connext/wms/service/impl/ExceptionServiceImpl.java
@@ -4,6 +4,7 @@ import com.connext.wms.dao.OutRepertoryMapper;
 import com.connext.wms.entity.*;
 import com.connext.wms.service.ExceptionService;
 import com.connext.wms.util.Page;
+import com.connext.wms.util.PageSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,36 +21,30 @@ public class ExceptionServiceImpl implements ExceptionService {
     private OutRepertoryMapper outRepertoryMapper;
 
     //分页查询所有异常订单信息返回列表
+    @Override
     public Page selectByPage(Integer currPage){
         List<OutRepertory> list = outRepertoryMapper.selectByPage((currPage-1)*Page.PAGE_SIZE, Page.PAGE_SIZE);
         OutRepertoryExample example = new OutRepertoryExample();
         String status = "%shipException%";
         example.or().andOutRepoStatusLike(status);
-        return byPage(currPage,list,example);
+        return PageSet.setPage(list,currPage,outRepertoryMapper.countByExample(example));
     }
 
     //查找关键字查找异常订单
+    @Override
     public Page selectByExampleToKey(Integer currPage,String key){
         String newKey = "%" + key + "%";
         List<OutRepertory> list = outRepertoryMapper.selectByKey1(newKey,(currPage-1)*Page.PAGE_SIZE, Page.PAGE_SIZE);
         OutRepertoryExample example = new OutRepertoryExample();
         example.or().andOutRepoIdLike(newKey);
-        return byPage(currPage,list,example);
+        return PageSet.setPage(list,currPage,outRepertoryMapper.countByExample(example));
     }
 
     //点击查看异常订单详情
+    @Override
     public OutRepertory selectByPrimaryKey(Integer id){
         return outRepertoryMapper.selectByPrimaryKey(id);
     }
 
-    //分页实现方法
-    Page byPage(Integer currPage,List<OutRepertory> list,OutRepertoryExample example){
-        Page page = new Page();
-        page.setTotalCount((long)outRepertoryMapper.countByExample(example));
-        page.setCurrPage(currPage);
-        page.init();
-        page.setData(list);
-        return page;
-    }
 
 }
