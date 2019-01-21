235690fc8b3c3b09af4d088551f344453174eabe
xmxxmxxmxxmxxmx <1695205904@qq.com>
2019-01-21 15:03:35 +0800

    增加日志，并且对发货信息为空的出库单做出判断：不允许发货操作！

--- src/main/java/com/connext/wms/aop/outRepoAnnotation.java
+++ src/main/java/com/connext/wms/aop/outRepoAnnotation.java
@@ -11,6 +11,6 @@ import java.lang.annotation.Target;
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface outRepoAnnotation {
+public @interface OutRepoAnnotation {
     String value() default "";
 }
